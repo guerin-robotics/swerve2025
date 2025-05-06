0d6cc9ac553a2f444a09fdafd796a29ccc72428b
EitherAura91599 <alexjmcminn@hotmail.com>
2025-05-06 12:43:20 -0400

    Removed All Extra Code for Practice Bot

--- src/main/java/frc/robot/Sequences.java
+++ src/main/java/frc/robot/Sequences.java
@@ -3,20 +3,5 @@ package frc.robot;
 import frc.robot.subsystems.*;
 
 public class Sequences {
-    public static void removeL2Algae() {
-        Elevator.toPosition(Constants.elevator.algaeLevel.L2);
-        Effector.algaeEffectorUp(0.2);
-        Elevator.toPosition(Constants.elevator.algaeLevel.L2 - 3);
-    }
-
-    public static void removeL3Algae() {
-        Elevator.toPosition(Constants.elevator.algaeLevel.L3);
-        Effector.algaeEffectorUp(0.2);
-        Elevator.toPosition(Constants.elevator.algaeLevel.L3 - 3);
-    }
-
-    public static void scoreL1Coral() {
-        Elevator.toPosition(Constants.elevator.level.L1);
-        Effector.asymmetricalOuttake(null, null);
-    }
+    
 }
