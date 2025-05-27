0593cfa1502552bb1383edb1d5bce5ed20ec5985
Parker Pulfer <parker.pulfer@icloud.com>
2025-05-27 10:40:04 -0400

    removed the reset pose when enabling telop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,7 +59,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        resetPose();
 
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
